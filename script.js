87804fa6128c84caece78a5a983a74bf99461b55
Aarthi <aarthikumar1299@gmail.com>
2023-06-08 18:43:56 +0530

    done the product filter

--- script.js
+++ script.js
@@ -48,54 +48,11 @@ function move_to(){
    
 // });
 
-// carousel
-
-
-    // const container = document.getElementById("carousel-container");
-    // const carousel = document.getElementById("carousel");
-    // const products = carousel.getElementsByClassName("product");
-
-    // let currentIndex = 0;
-
-    // function scrollToProduct(index) {
-    //   if (index < 0 || index >= products.length) {
-    //     return;
-    //   }
-
-    //   currentIndex = index;
-    //   carousel.style.transform = `translateX(-${currentIndex * ( 200/ products.length)}%)`;
-    //   console.log(currentIndex,products.length   )
-    // }
-
-    // // Function to scroll the carousel to the next set of products
-    // function scrollNext() {
-    //     const lastIndex = products.length - 4;
-    //     const nextIndex = currentIndex + 1;
-      
-    //     if (nextIndex <= lastIndex) {
-    //       scrollToProduct(nextIndex);
-    //     }
-    //   }
-
-
-    // // Function to scroll the carousel to the previous set of products
-    // function scrollPrev() {
-    //     const prevIndex = currentIndex - 1;
-      
-    //     if (prevIndex >= 0) {
-    //       scrollToProduct(prevIndex);
-    //     }
-    //   }
-
-    // // Add event listeners to scroll buttons
-    // document.getElementById("next-button").addEventListener("click", scrollNext);
-    // document.getElementById("prev-button").addEventListener("click", scrollPrev);
-
-    filterSelection("all")
+filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
+  if (c == "all") c = "MandR";
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
@@ -115,6 +72,8 @@ function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
+
+  console.log(arr1.length,arr2)
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
       arr1.splice(arr1.indexOf(arr2[i]), 1);     
@@ -133,3 +92,46 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// filterSelection("all")
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("filterDiv");
+//   if (c == "all") c = "MandR";
+//   for (i = 0; i < x.length; i++) {
+//     w3RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//   }
+// }
+
+// function w3AddClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+//   }
+// }
+
+// function w3RemoveClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);     
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
+
+// // Add active class to the current button (highlight it)
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function(){
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
